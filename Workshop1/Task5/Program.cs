7d92f0c4f9bdc49ee084821174ed7f4721ebfd88
antondubov <antondubov1@yandex.ru>
2022-08-28 23:04:49 +0300

    Выполнены задания с первого семинара

---
+++ Workshop1/Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 5: Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+//4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+//2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Здравствуйте, я программа по нахождению всех четных чисел в диапазоне [-N, N]");
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int index = -numberN;
+
+while(index < numberN+1)
+{   
+    if (index == numberN)
+    {
+        Console.Write(index);
+    }
+    else
+    {
+        Console.Write(index + ", ");} 
+    index++;  
+}
---
+++ Workshop1/Task8/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Здравствуйте, я программа по нахождению всех четных чисел в диапазоне [1, N]");
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while(index < numberN+1)
+{
+    if (index % 2 == 0)
+    {
+        if (index == numberN)
+        {Console.Write(index);
+        }
+        else
+        {Console.Write(index + ", ");}
+        
+        index++;
+    }
+    else
+    {
+        index++;
+    }
+}
