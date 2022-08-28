d3ab5c712a33bc63eba351b21ea09e1fde0da205
antondubov <antondubov1@yandex.ru>
2022-08-28 23:11:37 +0300

    Все задачи из второго семинара выполнены

---
+++ Workshop2/Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Здравствуйте, я программа по нахождению второй цифры из числа");
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+string strNumberN = Convert.ToString(numberN);
+Console.Write("Вторая цифра числа введенного числа - " + strNumberN[1]);
---
+++ Workshop2/Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Здравствуйте, я программа по нахождению третьей цифры из числа");
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+string strNumberN = Convert.ToString(numberN);
+int lenght = strNumberN.Length;
+if(lenght < 3)
+{
+    Console.Write("Введенное число не является трехзначным");
+}
+else 
+{
+    Console.Write("Третья цифра введенного числа - " + strNumberN[2]);
+}
