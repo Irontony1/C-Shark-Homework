40f793da0c392a56a4dfefb554efc2e51b3832cb
antondubov <antondubov1@yandex.ru>
2022-08-29 23:58:34 +0300

    Выполнена вторая задача

---
+++ Workshop4/Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int SumNumbers(int number)
+{   int result = 0;
+    while(number > 0)
+    {
+        int tmp = number % 10;
+        result += tmp;
+        number = number / 10;
+    }
+
+    return result;
+}
+
+Console.WriteLine("Здравствуйте, я сложу все цифры в числе");
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumNumbers(number));
