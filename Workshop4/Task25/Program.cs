b0c7b04fd13b56fcd5da7af41aa1f34928e85f2d
antondubov <antondubov1@yandex.ru>
2022-08-29 23:29:01 +0300

    Выполнена первая задача

---
+++ Workshop4/Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+double LevelNumber(int number, int level)
+{
+    return Math.Pow( number,level );
+}
+
+
+Console.WriteLine("Здравствуйте, я помогу возвести число в степень");
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("В какую степень возвести: ");
+int lvl = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Получилось : " + LevelNumber( number,lvl ));
