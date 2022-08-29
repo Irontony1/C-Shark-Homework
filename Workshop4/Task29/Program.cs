462a4529a25a98bdbd401b324b8f0e36b88e072b
antondubov <antondubov1@yandex.ru>
2022-08-30 00:26:43 +0300

    Все задания выполнены

---
+++ Workshop4/Task29/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+void FillArray(int[] array)
+{  
+    int size = array.Length;
+    for (int i = 0; i < size; i++ )
+    {
+        array[i] = new Random().Next(1,100);
+        if (i == 0)
+            Console.Write("[ " + array[i] + " , ");
+        else if (i >= 1 && i <= size-2)
+            Console.Write( array[i] + " , ");
+        else if (i == size-1)
+            Console.Write( array[i] + " ]");
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
+
